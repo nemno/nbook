4eac84d24938d71b76b2ecde71049cb335f66df9
Nemno <nono.nemes@gmail.com>
2010-12-27 21:08:16 +0100

    Elso commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nbook::Application.config.secret_token = '0bdcb8f1f22b3772ecb7a58d8353b5ddf3c1c262f565ae73c8cac9709238d7bc49cad79a55c588a43bf0519394f5b1bd8b63fda0d3525b2427ae78cd95fada5f'
